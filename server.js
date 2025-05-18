04b925a1000637ce1bf5da47bd22beaed30d050a
Haseebsid42 <133566587+Haseebsid42@users.noreply.github.com>
2025-05-18 17:34:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const qrcode = require('qrcode');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Directories
 const uploadDir = path.join(__dirname, 'uploads');
